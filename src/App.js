52a4bb2e9cbb8dfef1d9cd5822692c2070e8bd9f
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-08 23:14:05 +0900

    TaskCardに必要なcomponent作成（箱だけ）

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { TackCards } from "./components/tasks/TackCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TackCards />
     </div>
   );
 }
---
+++ src/components/tasks/TaskCard.jsx
@@ -0,0 +1,15 @@
+import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
+import { TaskAddInput } from "./TaskAddInput";
+import { TaskCardTitle } from "./TaskCardTitle";
+import { Tasks } from "./Tasks";
+
+export const TaskCard = () => {
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  );
+};
