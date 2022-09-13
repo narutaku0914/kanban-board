981b2f43b36d356252f75c3080c984a0bc23ef5c
narutaku0914 <tkm.nrsw@gmail.com>
2022-09-13 21:51:08 +0900

    TaskCardの削除ボタン作成

--- src/components/tasks/button/AddTaskCardButton.jsx
+++ src/components/tasks/button/AddTaskCardButton.jsx
@@ -1,7 +1,18 @@
-export const AddTaskCardButton = () => {
+import { v4 as uuidv4 } from "uuid";
+
+export const AddTaskCardButton = ({ taskCardsList, setTaskCardsList }) => {
+  const addTaskCard = () => {
+    const taskCardId = uuidv4();
+    setTaskCardsList([
+      ...taskCardsList,
+      { id: taskCardId, draggableId: `card${taskCardId}` },
+    ]);
+  };
   return (
     <div>
-      <button className="addTaskCardButton">+</button>
+      <button className="addTaskCardButton" onClick={addTaskCard}>
+        +
+      </button>
     </div>
   );
 };
--- src/components/tasks/button/TaskCardDeleteButton.jsx
+++ src/components/tasks/button/TaskCardDeleteButton.jsx
@@ -1,3 +1,19 @@
-export const TaskCardDeleteButton = () => {
-  return <div></div>;
+export const TaskCardDeleteButton = ({
+  taskCard,
+  taskCardsList,
+  setTaskCardsList,
+}) => {
+  const taskCardDelete = (id) => {
+    setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
+  };
+  return (
+    <div>
+      <button
+        className="taskCardDeleteButton"
+        onClick={() => taskCardDelete(taskCard.id)}
+      >
+        <i className="fa-solid fa-xmark"></i>
+      </button>
+    </div>
+  );
 };
